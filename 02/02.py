6a9505b2553b624d8750d359337c05adc4650afa
AEDavid <dschmitt@angstromengineering.com>
2024-12-02 12:36:40 -0500

    second half

--- 02/02.py
+++ 02/02.py
@@ -1,23 +1,24 @@
 def is_safe(report):
     last = report[0]
+    faultyReports = 0
     ascending = report[0] < report[1]
     for i in range(1, len(report)):
         if ascending:
             if last > report[i]:
                 print(str(last) + ">" + str(report[i]))
-                return False
+                faultyReports += 1
             if last == report[i] or last + 3 < report[i]:
                 print(str(last) + "asc == or + 3 " + str(report[i]))
-                return False
+                faultyReports += 1
         else:
             if last < report[i]:
                 print(str(last) + "<" + str(report[i]))
-                return False
+                faultyReports += 1
             if last == report[i] or last > report[i] + 3:
                 print(str(last) + "desc == or +3 " + str(report[i]))
-                return False
+                faultyReports += 1
         last = report[i]
-    return True
+    return faultyReports <= 1
 
 
 file1 = open("02/input.txt", "r")
